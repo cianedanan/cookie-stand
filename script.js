43f4bdf2a141279b8e2513425ee6b630f86798d1
Cianenry Danan <cianenryd@gmail.com>
2022-08-24 09:47:35 -0700

    Got some kind of table going.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00am', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
+let hours = ['Locations', '6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00am', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', 'Daily Total'];
 
 let tBody = document.getElementById('cookieData');
+
+let tRow = document.getElementById('cookieData');
+
+// let tData = document.getElementById('cookieData');
+
+let grandTotalCookies = 0;
 
 function Store(location, min, max, avgCookie){
   this.location = location;
@@ -34,4 +40,49 @@ for (let key in locations){
   locations[key].calcCookies();
 }
 
+function dailyCookies(){
+  for (let key in locations){
+    let tData = document.createElement('td');
+    tData.textContent = locations[key].totalCookies;
+    grandTotalCookies += locations[key].totalCookies;
+    tBody.appendChild(tData);
+  }
+}
+dailyCookies();
 
+function renderBody(){
+  for(let key in locations){
+    tRow = document.createElement('tr');
+    tBody.appendChild(tRow);
+
+    let tHead = document.createElement('th');
+    tHead.textContent = locations[key].location;
+    tBody.appendChild(tHead);
+
+    for(let i in locations[key].numberOfCookiesPerHour){
+      let tData = document.createElement('td');
+      tData.textContent = locations[key].numberOfCookiesPerHour[i];
+      console.log(locations[key].numberOfCookiesPerHour[i]);
+      tBody.appendChild(tData);
+    }
+
+  }
+}
+
+function renderHeader(){
+  tRow = document.createElement('tr');
+  tBody.appendChild(tRow);
+  for (let i = 0; i < hours.length; i++){
+    let tHead = document.createElement('th');
+    tHead.textContent = hours[i];
+    tBody.appendChild(tHead);
+  }
+}
+
+
+renderHeader();
+renderBody();
+
+// function renderFooter(){
+//   kjlk;
+// }
